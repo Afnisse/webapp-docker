8b849112aa109616db6cce937032d41ec8f6041f
Oussama Elgoumri <ktsnepyg9igfz1@gmail.com>
2017-10-12 10:52:51 +0100

    Initial Setup

---
+++ readme.md
@@ -0,0 +1,12 @@
+[Gomri Tech website source code](https://gomri.tech)
+
+# Requirements
+
+* docker `Docker version 17.09.0-ce, build afdb6d4`
+* docker-compose `docker-compose version 1.16.1, build 6d1ac21`
+
+# Installation
+
+All you need to do is run `./install` and it will take care of everything ;)
+
+after the installation is complete, go to your browser, and type `http://gomri-tech.dev`
---
+++ slugs/laravel-setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo ------------------------------------------------------ Composer packages --
+if [[ -d vendor ]]; then
+    composer update
+else
+    composer install
+fi
+
+echo ----------------------------------------------------- Populate .env file --
+php artisan key:generate
+
+sed -i "s/%APP_NAME%/${X_APP_NAME}/" .env
+sed -i "s/%APP_ENV%/${X_APP_ENV}/" .env
+sed -i "s/%APP_DEBUG%/${X_APP_DEBUG}/" .env
+sed -i "s/%APP_LOG_LEVEL%/${X_APP_LOG_LEVEL}/" .env
+sed -i "s/%APP_URL%/${X_APP_URL}/" .env
+sed -i "s/%DB_CONNECTION%/${X_DB_CONNECTION}/" .env
+sed -i "s/%DB_HOST%/$1/" .env
+sed -i "s/%DB_PORT%/${X_DB_PORT}/" .env
+sed -i "s/%DB_DATABASE%/${X_DB_DATABASE}/" .env
+sed -i "s/%DB_USERNAME%/${X_DB_USERNAME}/" .env
+sed -i "s/%DB_PASSWORD%/${X_DB_PASSWORD}/" .env
+sed -i "s/%BROADCAST_DRIVER%/${X_BROADCAST_DRIVER}/" .env
+sed -i "s/%CACHE_DRIVER%/${X_CACHE_DRIVER}/" .env
+sed -i "s/%SESSION_DRIVER%/${X_SESSION_DRIVER}/" .env
+sed -i "s/%SESSION_LIFETIME%/${X_SESSION_LIFETIME}/" .env
+sed -i "s/%QUEUE_DRIVER%/${X_QUEUE_DRIVER}/" .env
+sed -i "s/%REDIS_HOST%/$2/" .env
+sed -i "s/%REDIS_PASSWORD%/${X_REDIS_PASSWORD}/" .env
+sed -i "s/%REDIS_PORT%/${X_REDIS_PORT}/" .env
+
+echo ----------------------------------------------- Edit configuration files --
+sed -i "s/'client' => 'predis',/'client' => 'phpredis',/" config/database.php
+sed -i "s/'prefix' => 'laravel',/'prefix' => '${CACHE_PREFIX}',/" config/cache.php
+
+echo ------------------------------------------------------------- Deployment --
+if [ $APP_ENV = 'deployment' ]; then
+    composer install --optimize-autoloader
+    php artisan config:cache
+    php artisan route:cache
+fi
+
+echo ------------------------------------------------------ Setup permissions --
+find /var/www/html -type f -exec chmod 644 {} \;
+find /var/www/html -type d -exec chmod 755 {} \;
+find /var/www/html/storage -type d -exec chmod 775 {} \;
+find /var/www/html/storage -type f -exec chmod 664 {} \;
+find /var/www/html/bootstrap/cache -type d -exec chmod 775 {} \;
+find /var/www/html/bootstrap/cache -type f -exec chmod 664 {} \;
+
+if [ ! -f storage/logs/laravel.log ]; then
+    touch storage/logs/laravel.log
+fi
+
+chmod 664 storage/logs/laravel.log
+chmod +x setup.sh
+
+chown -R `tail -n1 /etc/passwd | cut -d':' -f1`:www-data /var/www/html
